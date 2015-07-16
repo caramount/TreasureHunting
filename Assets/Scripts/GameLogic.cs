bd941766d0dd3c9e35c85b71c9f2f1927be6c5cc
Ryan Cavanaugh <ryan.cavanaugh@me.com>
2015-07-15 23:34:12 -0400

    Turn in version

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	public Transform player;
+	public Text youWin;
+	public Text hint;
+
+	// Use this for initialization
+	void Start () {
+		youWin.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && 
+			Vector3.Distance(player.position, transform.position) < 5)
+		{
+			youWin.enabled = true;
+		}
+		if (player.position.x < -50f)
+		{
+			hint.text = "Psst! You're going the wrong way, head east!";
+		}
+		if (player.position.z < -50f)
+		{
+			hint.text = "Ummm you should probably head north NOT south ya dummy";
+		}
+
+		if (player.position.x > 330f)
+		{
+			hint.text = "You're too far east! Back it up just a little bit";
+		}
+		else if (player.position.z > 320f)
+		{
+			hint.text = "You're too far north! Back it up just a little bit";
+		}
+		else if (player.position.x > 50f && player.position.z > 50f)
+		{
+			hint.text = "You're on the right track! Keep it up!";
+		}
+
+		if (player.position.x < 50f && player.position.x > -50f &&
+			player.position.z < 50f && player.position.z > -50f)
+		{
+			hint.text = "Currently in starting area.\nYou're looking for a sphere. When you see it, give it a little love tap and press [SPACE]! Some hints to start you off: Press [SHIFT] to go fast and follow the rocks.";
+		}
+	}
+}
---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour {
+
+	public float speed = 4.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// This code is framerate dependent (behavior will be different base on framerate)
+		// transform.position += new Vector3(0, 0, 1f);
+
+		// This code is framerate independent (Time.deltaTime == time between frame)
+		// 60 FPS >> Time.deltaTime = ~16 ms
+		// 10 FPS >> Time.deltaTime = ~100 ms
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += new Vector3(0, 0, 1f) * Time.deltaTime * speed;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += new Vector3(-1f, 0, 0) * Time.deltaTime * speed;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += new Vector3(0, 0, -1f) * Time.deltaTime * speed;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += new Vector3(1f, 0, 0) * Time.deltaTime * speed;
+		}
+		
+		// Could also do this by creating shift speeed variable and setting it 
+		// equal on GetKey a la:
+		//	 if (Input.GetKey (KeyCode.LeftShift)){
+		//		actualSpeed = shiftSpeed;
+		//	 } else {
+		//		actualSpeed = speed;
+		//	 } 
+		// you would then change speed above to actualSpeed
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			speed *= 2;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			speed = speed/2;
+		}
+	}
+}
